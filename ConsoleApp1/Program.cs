90050ccea331273c93c9411077e32686e2d6ad31
Kim SeungHyun <korafa@naver.com>
2024-04-04 15:51:01 +0900

    초기 커밋.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+class CSharpMasterClass
+{
+    static void Main(string[] args)
+    {
+        Stack<int> stack = new Stack<int>();
+
+        stack.Push(1);
+        stack.Push(2);
+        stack.Push(3);
+
+        while (stack.Count > 0)
+        {
+            Console.WriteLine($"이번에 제거된 숫자는? {stack.Pop()}");
+            Console.WriteLine($"스택안에 남은 숫자갯수는? {stack.Count}");
+        }
+
+        int[] numbers = { 8, 2, 3, 4, 7, 6, 2 };
+
+        Stack<int> myStack = new Stack<int>();
+
+        foreach (var num in numbers)
+        {
+            myStack.Push(num);
+        }
+        int[] reverseNum = new int[numbers.Length];
+        
+        for (int i = 0; i < reverseNum.Length; i++)
+        {
+            reverseNum[i] = myStack.Pop();
+            Console.Write(" "+reverseNum[i]);
+        }
+    }
+}
+            
---
+++ ConsoleApp1/Queue.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+class PractiseQueue
+{
+    static void Main(string[] args)
+    {
+        Queue<int> queue = new Queue<int>();
+        queue.Enqueue(1);
+        queue.Enqueue(2);
+        queue.Enqueue(3);
+
+        while (queue.Count > 0)
+        {
+            Console.WriteLine($"이번에 {queue.Dequeue()}가 제거됨");
+            Console.WriteLine($"제거되고 남은 갯수는 : {queue.Count}");
+        }
+    }
+}
